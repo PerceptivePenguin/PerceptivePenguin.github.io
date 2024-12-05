a164ba8c1a935923b6a778ce8769ff7d59d5dc95
PerceptivePenguin <93648437+PerceptivePenguin@users.noreply.github.com>
2024-12-05 15:24:25 +0800

    Delete js/custom directory

--- js/custom/imagespacefix.js
+++
@@ -1,9 +0,0 @@
-const images = document.querySelectorAll("img");
-
-images.forEach(function(img) {
-  let currentSrc = img.src;
-
-  currentSrc = currentSrc.replace(/%20/g, ' ');
-
-  img.src = currentSrc;
-});
